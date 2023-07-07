8e6546bb761b309b036e7e2000ebc382196fe6c9
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-07-07 18:22:53 +0900

    userモデルにバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :age
+    validates :height
+    validates :weight
+    validates :essential_cal
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :gender_id
+    validates :level_id
+  end
+  
 end
