7bf15effb10eac90d29668805ddf3e177796c2c4
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-07-10 21:24:32 +0900

    トップページを訂正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,9 +3,28 @@ class CalendarsController < ApplicationController
   # before_action: move_to_index, only:[:show, :edit, :destroy]
 
   def index
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      start_date = Date.today - 6.days
+      end_date = Date.today
+      @calendars = Calendar.where(date: start_date..end_date)
+    end
+  end
+
+  def new
+    @calendar = Calendar.new
+  end
+
+  def create
+  end
+
+  def show
   end
 
   private
+  def create_calendar
+  end
+  
   # def move_to_index
   #   unless current_user.id == @calendar.
   #     redirect_to "/"
