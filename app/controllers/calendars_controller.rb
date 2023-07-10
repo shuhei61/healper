b88da5330bc842e720c446f4e8a18bfc2c195a4c
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-07-10 15:42:01 +0900

    遷移条件の記述

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,21 @@
 class CalendarsController < ApplicationController
+  # before_action: move_to_session, only:[:new, :edit]
+  # before_action: move_to_index, only:[:show, :edit, :destroy]
 
   def index
   end
-  
+
+  private
+  # def move_to_index
+  #   unless current_user.id == @calendar.
+  #     redirect_to "/"
+  #   end
+  # end
+
+  # def move_to_session
+  #   unless user_signed_in?
+  #     redirect_to "/"
+  #   end
+  # end
+
 end
