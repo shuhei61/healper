cd8001d5bfa6bdd61a2c8b3809ce1c013cb844a1
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-07-13 19:27:36 +0900

    user登録時にwantsテーブルを自動生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to:"calendars#index"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'custom_registrations'
+  }
   resources :calendars
   resources :foods
 end
